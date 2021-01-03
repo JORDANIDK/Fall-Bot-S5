ac32583065642b662406eccc68df8c11edaa0fab
jordan123pal <iamleyendas@gmail.com>
2021-01-03 19:24:19 +0000

    Update Run with admin for instal libs.bat
    add for new gui

--- Run with admin for instal libs.bat
+++ Run with admin for instal libs.bat
@@ -2,4 +2,7 @@ py -m pip install --upgrade pip
 py -m pip install pyautogui
 py -m pip install opencv-python
 py -m pip install psutil
+py -m pip install requests
+py -m pip install pyQt5
+py -m pip install pyQt5-tools
 pause
