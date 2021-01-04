80592b6ba218018c7f9bd972c5ec48e3d11bbbec
jordan123pal <iamleyendas@gmail.com>
2021-01-04 02:04:43 +0000

    Delete Run with admin for instal libs.bat

--- Run with admin for instal libs.bat
+++
@@ -1,8 +0,0 @@
-py -m pip install --upgrade pip
-py -m pip install pyautogui
-py -m pip install opencv-python
-py -m pip install psutil
-py -m pip install requests
-py -m pip install pyQt5
-py -m pip install pyQt5-tools
-pause
